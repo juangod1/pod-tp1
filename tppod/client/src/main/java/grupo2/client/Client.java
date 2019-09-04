fc92ba839f965f24f89dc8152cba31ca45d76028
pabloito <pradnic@itba.edu.ar>
2019-09-04 20:25:44 -0300

    mvn archetype

---
+++ tppod/client/src/main/java/grupo2/client/Client.java
@@ -0,0 +1,12 @@
+package grupo2.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+    private static Logger logger = LoggerFactory.getLogger(Client.class);
+
+    public static void main(String[] args) {
+        logger.info("tppod Client Starting ...");
+    }
+}
---
+++ tppod/server/src/main/java/grupo2/server/Server.java
@@ -0,0 +1,12 @@
+package grupo2.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("tppod Server Starting ...");
+    }
+}
